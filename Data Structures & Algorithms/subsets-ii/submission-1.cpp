dc1a6088729cba5f67ba9d4bc0331026ac6bb937
GUDDETI SREETEJA <84274198+sreeteja2006@users.noreply.github.com>
2026-06-18 10:42:39 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    void dfs(int i, vector<int>& nums, vector<vector<int>>& res, vector<int>& subset){
+        res.push_back(subset);
+
+        for(int j = i; j < nums.size(); j++){
+            if(j > i && nums[j] == nums[j-1]) continue;
+
+            subset.push_back(nums[j]);
+            dfs(j + 1, nums, res, subset);
+            subset.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());  
+        vector<vector<int>> res;
+        vector<int> subset;
+        dfs(0, nums, res, subset);
+        return res;
+    }
+};
